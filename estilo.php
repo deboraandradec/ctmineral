cf058802f45306ae4f46e3a69b3be138fbe08c6e
debora de andrade <deboradeandrade13@gmail.com>
2022-05-12 22:17:04 -0300

    first commit

---
+++ estilo.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-type: text/css");
+
+$cor_fundo = '#CCCCFF';
+$cor_texto = '#003333';
+$imagem_link = '../img/link.jpg'
+?>
+
+
+
+ .container{ 
+
+
+}
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.php" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>ctmineral inicio</title>
+</head>
+<body>
+<div class="forma1"> 
+</div>
+
+<div class="container">
+  <header class="d-flex justify-content-center py-3">
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Início</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Institucional</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Nossos serviços</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Infraestrutura</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+    </ul>
+  </header>
+</div>
+</body>
+</html>
